824a6706b15ae009b9b6daa331dcec2f3861d853
humlaye <77547903+humlaye@users.noreply.github.com>
2023-06-05 11:26:10 +0300

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,23 +1,23 @@
 
 #include <MG811.h>
 
-// Pin tanımlamaları
-const int CO2SensorPin = A0;  // CO2 sensörü bağlantı pini
-const int TemperatureSensorPin = A1;  // Sıcaklık sensörü bağlantı pini
-const int IRSensorPin = 6;          // Kızılötesi sensör bağlantı pini
-const int WaterMotorPin1 = 7;       // Su motoru 1 bağlantı pini
-const int WaterMotorPin2 = 8;       // Su motoru 2 bağlantı pini
-const int BuzzerPin = 9;     // Buzzer bağlantı pini
-const int MotorPin1 = 10;    // Motor 1 bağlantı pini
-const int MotorPin2 = 11;    // Motor 2 bağlantı pini
+// Pin definitions
+const int CO2SensorPin = A0;  // CO2 sensor connection pin
+const int TemperatureSensorPin = A1;  //Temperature sensor connection pin
+const int IRSensorPin = 6;          // Infrared sensor connection pin
+const int WaterMotorPin1 = 7;       // Water motor 1 connection pin
+const int WaterMotorPin2 = 8;       // Water motor 2 connection pin
+const int BuzzerPin = 9;     //Buzzer connection pin
+const int MotorPin1 = 10;    // Motor 1 connection pin
+const int MotorPin2 = 11;    // Motor 2 connection pin
 
 
-// Sensör nesnesi oluştur
+// Create sensor object
 MG811 co2Sensor(CO2SensorPin);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
-  // Pin modlarını ayarla
+  // Set pin modes
   pinMode(BuzzerPin, OUTPUT);
   pinMode(MotorPin1, OUTPUT);
   pinMode(MotorPin2, OUTPUT);
@@ -25,29 +25,29 @@ void setup() {
   pinMode(WaterMotorPin1, OUTPUT);
   pinMode(WaterMotorPin2, OUTPUT);
   
-  // Sıcaklık sensörünü başlat
+  // Start temperature sensor
   mlx.begin();
   
-  // Serial haberleşmesini başlat 
+  // start serial communication
   Serial.begin(9600);
 }
 
 void loop() {
-  // CO2 seviyesini ölç
+  // Measure CO2 level
   int co2Level = co2Sensor.readCO2();
-    // Sıcaklık ölçümünü yap
+    // Temperature measurement
   float temperature = mlx.readObjectTempC();
 
-  // CO2 seviyesini seri portta görüntüle 
-  Serial.print("CO2 Seviyesi: ");
+  // Display CO2 level on serial port
+  Serial.print("CO2 level: ");
   Serial.println(co2Level);
-    // Sıcaklık değerini seri portta görüntüle 
+    // Display temperature value on serial port
   Serial.print("Sıcaklık: ");
   Serial.println(temperature);
 
-  // Eşik değeri kontrolü yap
+  // check co2 level threshold
   if (co2Level > 1000) {  
-    // Buzzer'ı 5 saniye boyunca iki kez çal
+    // Ring the buzzer twice for 5 seconds
     for (int i = 0; i < 2; i++) {
       digitalWrite(BuzzerPin, HIGH);
       delay(5000);
@@ -55,23 +55,18 @@ void loop() {
       delay(5000);
     }
     
-    // Motorları çalıştır
+    // start the motors
     digitalWrite(MotorPin1, HIGH);
     digitalWrite(MotorPin2, LOW);
-    
-    // CO2 seviyesi düşene kadar motorları çalıştır
-    while (co2Level > 1000) {
-      co2Level = co2Sensor.readCO2();
-    }
-    
-    // Motorları durdur
+  }else{
+    // stop motors
     digitalWrite(MotorPin1, LOW);
     digitalWrite(MotorPin2, LOW);
   }
   
-    // Eşik değeri kontrolü yap
+    // check for temperature
   if (temperature > 30.0) {  
-    // Buzzer'ı 5 saniye boyunca iki kez çal
+    // BRing the buzzer twice for 5 seconds
     for (int i = 0; i < 2; i++) {
       digitalWrite(BuzzerPin, HIGH);
       delay(5000);
@@ -79,23 +74,18 @@ void loop() {
       delay(5000);
     }
     
-    // Kızılötesi sensörü kontrol et
+    // Check infrared sensor
     if (digitalRead(IRSensorPin) == HIGH) {
-      // Yangın durumu algılandı, su motorlarını çalıştır
+      // Fire condition detected, start water motor
       digitalWrite(WaterMotorPin1, HIGH);
       digitalWrite(WaterMotorPin2, LOW);
     }
-    
-    // Yangın durumu sona erene kadar bekle
-    while (digitalRead(IRSensorPin) == HIGH) {
-      
-    }
-    
-    // Yangın durumu sona erdi, su motorlarını durdur
+  }else{  
+    // Fire condition over, stop water motors
     digitalWrite(WaterMotorPin1, LOW);
     digitalWrite(WaterMotorPin2, LOW);
   }
   
   
-  delay(1000);  // Her saniyede bir ölçüm yapılmasını sağlar
+  delay(1000); 
 }
